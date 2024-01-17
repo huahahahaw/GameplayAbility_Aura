dd3e2bdfede2df17422620a4fe324dbbd32b9529
huahahahaw <81372228+huahahahaw@users.noreply.github.com>
2024-01-17 19:15:13 +0800

    half submit

---
+++ Source/Aura/Public/UI/WidgetController/AuraWidgetController.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "AuraWidgetController.generated.h"
+
+class UAbilitySystemComponent;
+class UAttributeSet;
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraWidgetController : public UObject
+{
+	GENERATED_BODY()
+protected:
+	UPROPERTY(BlueprintReadOnly,Category = "WidgetContoller")
+	TObjectPtr<APlayerController> PlayerController;
+
+	UPROPERTY(BlueprintReadOnly, Category = "WidgetContoller")
+	TObjectPtr<APlayerState> PlayerState;
+
+	UPROPERTY(BlueprintReadOnly, Category = "WidgetContoller")
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+
+	UPROPERTY(BlueprintReadOnly, Category = "WidgetContoller")
+	TObjectPtr<UAttributeSet> AttributeSet;
+};
