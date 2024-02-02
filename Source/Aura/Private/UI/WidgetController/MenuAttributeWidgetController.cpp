8f71f30687847f420f2626f0d382832d5bc370f0
huahahahaw <81372228+huahahahaw@users.noreply.github.com>
2024-02-02 18:18:43 +0800

    无心学习提交

---
+++ Source/Aura/Private/UI/WidgetController/MenuAttributeWidgetController.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/WidgetController/MenuAttributeWidgetController.h"
+
+void UMenuAttributeWidgetController::BoradcastInitialValues()
+{
+}
+
+void UMenuAttributeWidgetController::BindCallbacksToDependencies()
+{
+}
